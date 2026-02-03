0850a918c3dc9ca03565176ff841cc5b1193c85e
Jesse Carnaxide <jcarnaxide@gmail.com>
2026-02-02 21:08:59 -0500

    working on bat model

--- first-3d-game/player/player.gd
+++ first-3d-game/player/player.gd
@@ -37,3 +37,16 @@ func _physics_process(delta):
 		velocity.y = lerp(velocity.y, 0.0, 0.5)
 	
 	move_and_slide()
+	
+	if Input.is_action_pressed("shoot") and %Timer.is_stopped():
+		shoot_bullet()
+
+
+func shoot_bullet():
+	const BULLET_3D = preload("uid://cuqmgsjic2t50")
+	var new_bullet = BULLET_3D.instantiate()
+	%Marker3D.add_child(new_bullet)
+	
+	new_bullet.global_transform = %Marker3D.global_transform
+	
+	%Timer.start()

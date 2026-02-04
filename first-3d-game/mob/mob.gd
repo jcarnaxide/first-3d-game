159cb0d6b89725312ef24e88ea67ed6482dea5b3
Jesse Carnaxide <jcarnaxide@gmail.com>
2026-02-03 22:59:45 -0500

    mobs follow player, take dmg, and die

--- first-3d-game/mob/mob.gd
+++ first-3d-game/mob/mob.gd
@@ -1,8 +1,10 @@
 extends RigidBody3D
 
+var health = 3
 var speed = randf_range(2.0, 4.0)
 
 @onready var bat_model = %bat_model
+@onready var timer = %Timer
 
 @onready var player = get_node("/root/Game/Player")
 
@@ -11,7 +13,25 @@ func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	direction.y = 0.0
 	linear_velocity = direction * speed
+	rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
 
 
 func take_damage():
+	if health == 0:
+		return
+	
 	bat_model.hurt()
+	
+	health -= 1
+	
+	if health == 0:
+		set_physics_process(false)
+		gravity_scale = 1.0
+		var direction = -1.0 * global_position.direction_to(player.global_position)
+		var random_upward_force = Vector3.UP * randf_range(1.0, 5.0)
+		apply_central_impulse(direction * 10.0 + random_upward_force)
+		timer.start()
+
+
+func _on_timer_timeout():
+	queue_free()
